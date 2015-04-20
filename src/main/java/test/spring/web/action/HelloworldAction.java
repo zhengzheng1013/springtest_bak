0ad358e06f6699160db5d2d218bfdb3b31e7f5a3
xuefeng0707 <xuefeng0707@hotmail.com>
2015-04-20 17:09:31 +0800

    helloworld

---
+++ src/main/java/test/spring/web/action/HelloworldAction.java
@@ -0,0 +1,23 @@
+package test.spring.web.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/helloworld")
+public class HelloworldAction {
+	
+	@RequestMapping("test")
+	public void test(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			response.getWriter().write("TEST page");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
